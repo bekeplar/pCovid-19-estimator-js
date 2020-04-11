db7a87393f5215430d1add6c5a611ce1c1b198ed
bekeplar <bekeplar@gmail.com>
2020-04-11 20:21:51 +0300

    add input label

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 
+
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    marginTop: theme.spacing(3),
   },
-
-  // Add form styling.
+  // Add form styling
   form: {
     width: '100%',
     marginTop: theme.spacing(3),
   },
+  formControl: {
+    width: '100%',
+  },
   submit: {
-    margin: theme.spacing(3, 1, 2),
+    margin: theme.spacing(3, 0, 2),
   },
 }));
-
 export default function Form() {
   const classes = useStyles();
 
@@ -53,101 +58,104 @@ export default function Form() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // console.log(state);
+    // eslint-disable-next-line no-console
+    console.log(state);
   };
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          COVID-19 ESTIMATOR
+        <Typography component="h1" variant="h5" style={{ textAlign: 'center' }}>
+          A simple web app to collect covid-19 pandemic Estimate.
         </Typography>
         <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
-                name="population"
-                variant="outlined"
                 required
                 fullWidth
-                id="population"
-                label="Population"
                 autoFocus
                 type="number"
-                data-population
-                value={state.population}
+                id="population"
+                name="population"
+                label="Population"
+                variant="outlined"
                 onChange={handleChange}
+                value={state.population}
+                inputProps={{ 'data-population': true }}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
-                variant="outlined"
                 required
                 fullWidth
+                type="number"
+                id="reported-cases"
+                variant="outlined"
                 name="reportedCases"
                 label="Reported Cases"
-                id="reported-cases"
-                type="number"
-                data-reported-cases
-                value={state.reportedCases}
                 onChange={handleChange}
+                value={state.reportedCases}
+                inputProps={{ 'data-reported-cases': true }}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
-                variant="outlined"
                 required
                 fullWidth
-                id="total-hospital-beds"
-                label="Total Hospital Beds"
-                name="totalHospitalBeds"
                 type="number"
-                data-total-hospital-beds
-                value={state.totalHospitalBeds}
+                variant="outlined"
+                id="total-hospital-beds"
+                name="totalHospitalBeds"
+                label="Total Hospital Beds"
                 onChange={handleChange}
+                value={state.totalHospitalBeds}
+                inputProps={{ 'data-total-hospital-beds': true }}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
-                variant="outlined"
                 required
                 fullWidth
+                type="number"
+                variant="outlined"
+                name="timeToElapse"
                 id="time-to-elapse"
                 label="Time To Elapse"
-                name="timeToElapse"
-                type="number"
-                data-time-to-elapse
-                value={state.timeToElapse}
                 onChange={handleChange}
+                value={state.timeToElapse}
+                inputProps={{ 'data-time-to-elapse': true }}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField
-                id="period-type"
-                select
-                fullWidth
-                label="Period Type"
-                name="periodType"
-                value={state.periodType}
-                onChange={handleChange}
-                variant="outlined"
-              >
-                <MenuItem value="days">Days</MenuItem>
-                <MenuItem value="weeks">Weeks</MenuItem>
-                <MenuItem value="months">Months</MenuItem>
-              </TextField>
+              <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="period-type-label">Period Type</InputLabel>
+                <Select
+                  id="period-type"
+                  name="periodType"
+                  variant="outlined"
+                  label="Period Type"
+                  onChange={handleChange}
+                  value={state.periodType}
+                  inputProps={{ 'data-period-type': true }}
+                >
+                  <MenuItem data-period-type="true" value="days">Days</MenuItem>
+                  <MenuItem data-period-type="true" value="weeks">Weeks</MenuItem>
+                  <MenuItem data-period-type="true" value="months">Months</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
           </Grid>
           <Button
-            type="submit"
             fullWidth
-            variant="contained"
-            color="primary"
             size="large"
-            className={classes.submit}
+            type="submit"
+            color="primary"
+            variant="contained"
             data-go-estimate
             disabled={!isValid}
+            className={classes.submit}
           >
             Collect Estimate
           </Button>
