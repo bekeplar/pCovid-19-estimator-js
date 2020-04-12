af5c0fabe3df81eba128a9fdccdc24cad70cf98a
bekeplar <bekeplar@gmail.com>
2020-04-12 23:02:12 +0300

    Add frontend

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
-import Grid from '@material-ui/core/Grid';
 import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
 import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -69,84 +65,97 @@ export default function Form() {
         <Typography component="h1" variant="h5" style={{ textAlign: 'center' }}>
           A simple web app to collect covid-19 pandemic Estimate.
         </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={handleSubmit}
+        >
+          <div className="data-form-inputs">
+            <span className="d-block padding-s">Population</span>
+            <input
+              variant="outlined"
+              required
+              fullWidth
+              id="population"
+              label="Population"
+              name="population"
+              type="number"
+              data-population
+              value={state.population}
+              onChange={handleChange}
+            />
+          </div>
+              &nbsp;
+          <div className="data-form-inputs">
+            <span className="d-block padding-s">Reported Cases</span>
+            <input
+
+              variant="outlined"
+              required
+              fullWidth
+              name="reportedCases"
+              label="Reported Cases"
+              id="reported-cases"
+              type="number"
+              data-reported-cases
+              value={state.reportedCases}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="data-form-inputs">
+            <span className="d-block padding-s">Total Hospital Beds</span>
+            <input
+
+
+              variant="outlined"
+              required
+              fullWidth
+              id="total-hospital-beds"
+              label="Total Hospital Beds"
+              name="totalHospitalBeds"
+              type="number"
+              data-total-hospital-beds
+              value={state.totalHospitalBeds}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="d-flex">
+            <div className="data-form-inputs">
+              <span className="d-block padding-s">Time To Elapse</span>
+              <input
+
+                variant="outlined"
                 required
                 fullWidth
-                autoFocus
-                type="number"
-                id="population"
-                name="population"
-                label="Population"
-                variant="outlined"
-                onChange={handleChange}
-                value={state.population}
-                inputProps={{ 'data-population': true }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                type="number"
-                id="reported-cases"
-                variant="outlined"
-                name="reportedCases"
-                label="Reported Cases"
-                onChange={handleChange}
-                value={state.reportedCases}
-                inputProps={{ 'data-reported-cases': true }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                type="number"
-                variant="outlined"
-                id="total-hospital-beds"
-                name="totalHospitalBeds"
-                label="Total Hospital Beds"
-                onChange={handleChange}
-                value={state.totalHospitalBeds}
-                inputProps={{ 'data-total-hospital-beds': true }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                type="number"
-                variant="outlined"
-                name="timeToElapse"
                 id="time-to-elapse"
                 label="Time To Elapse"
-                onChange={handleChange}
+                name="timeToElapse"
+                type="number"
+                data-time-to-elapse
                 value={state.timeToElapse}
-                inputProps={{ 'data-time-to-elapse': true }}
+                onChange={handleChange}
               />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel id="period-type-label">Period Type</InputLabel>
-                <Select
-                  id="period-type"
-                  name="periodType"
-                  variant="outlined"
-                  label="Period Type"
-                  onChange={handleChange}
-                  value={state.periodType}
-                  inputProps={{ 'data-period-type': true }}
-                >
-                  <MenuItem data-period-type="true" value="days">Days</MenuItem>
-                  <MenuItem data-period-type="true" value="weeks">Weeks</MenuItem>
-                  <MenuItem data-period-type="true" value="months">Months</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-          </Grid>
+            </div>
+              &nbsp;
+          </div>
+          <br />
+          <div className="data-form-inputs">
+            <span className="d-block padding-s">Period Type</span>
+            <Select
+              id="period-type"
+              name="periodType"
+              variant="outlined"
+              label="Period Type"
+              onChange={handleChange}
+              value={state.periodType}
+              inputProps={{ 'data-period-type': true }}
+            >
+              <MenuItem data-period-type="true" value="days">Days</MenuItem>
+              <MenuItem data-period-type="true" value="weeks">Weeks</MenuItem>
+              <MenuItem data-period-type="true" value="months">Months</MenuItem>
+            </Select>
+
+          </div>
           <Button
             fullWidth
             size="large"
