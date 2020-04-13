e1207e1ba56e10d594839adaa8cf090ef2f42bd8
bekeplar <bekeplar@gmail.com>
2020-04-13 12:35:32 +0300

    Add frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './component/Header';
-import Footer from './component/Footer';
+// import Footer from './component/Footer';
 import Form from './component/Form';
 import './App.css';
 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <Form />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
