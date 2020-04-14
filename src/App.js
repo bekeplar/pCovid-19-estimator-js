d9000b934e0e186f355e3149db8aeb23f41526c2
bekeplar <bekeplar@gmail.com>
2020-04-15 00:21:43 +0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Header from './component/Header';
-// import Footer from './component/Footer';
 import Form from './component/Form';
 import './App.css';
 
-// Display components in the app.
+
 function App() {
   return (
     <div className="App">
